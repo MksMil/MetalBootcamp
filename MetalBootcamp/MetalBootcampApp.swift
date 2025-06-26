338a8984caacf34bc3e2e0477977085309a1f9eb
MksMil <maksymmiliaiev@gmail.com>
2025-06-26 10:34:55 +0300

    Initial Commit

---
+++ MetalBootcamp/MetalBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetalBootcampApp.swift
+//  MetalBootcamp
+//
+//  Created by Миляев Максим on 26.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct MetalBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
