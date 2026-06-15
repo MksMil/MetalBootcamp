d0b5e4485fe90c74575da6848a4e15d9e5158397
MksMil <maksymmiliaiev@gmail.com>
2026-06-15 13:29:08 +0300

    try waves shader from shadertoy. add new heart sdf, add changing shape color, and change shape type. next: interpolation function to interpolate shapes change

---
+++ MetalBootcamp/Metal/MetalFunctions.h
@@ -0,0 +1,12 @@
+//
+//  MetalFunctions.h
+//  MetalBootcamp
+//
+//  Created by Миляев Максим on 13.06.2026.
+//
+
+#ifndef MetalFunctions_h
+#define MetalFunctions_h
+
+
+#endif /* MetalFunctions_h */
---
+++ MetalBootcamp/Metal/MetalFunctions.metal
@@ -0,0 +1,11 @@
+//
+//  MetalFunctions.metal
+//  MetalBootcamp
+//
+//  Created by Миляев Максим on 13.06.2026.
+//
+
+#include <metal_stdlib>
+using namespace metal;
+
+
